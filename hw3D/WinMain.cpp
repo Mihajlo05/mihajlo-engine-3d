8ea6410b25bbebca2ee36d9189796c7496098655
Mihajlo05 <mihajlosrec12@gmail.com>
2021-05-19 09:11:50 +0200

    Addde some message handling

--- hw3D/WinMain.cpp
+++ hw3D/WinMain.cpp
@@ -1,14 +1,41 @@
 #include <Windows.h>
+#include <string>
 
-LRESULT CALLBACK WndProc(HWND wnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static std::string title;
 	switch (uMsg)
 	{
 	case WM_CLOSE:
 		PostQuitMessage(0);
 		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_SHIFT)
+			MoveWindow(hWnd, 100, 100, 100, 100, 0);
+		if (wParam == 'E')
+			SetWindowText(hWnd, "New and Better MihajloEngine3D");
+		break;
+	case WM_KEYUP:
+		if (wParam == 'E')
+			SetWindowText(hWnd, "lol");
+		break;
+	case WM_CHAR:
+		if (wParam != VK_BACK)
+		{
+			title.push_back(wParam);
+		}
+		else if (title.size() != 0)
+		{
+			title.pop_back();
+		}
+		SetWindowText(hWnd, title.c_str());
+		break;
+	case WM_LBUTTONDBLCLK:
+		SetWindowText(hWnd, "DBLCKL");
+		break;
+
 	}
-	return DefWindowProc(wnd, uMsg, wParam, lParam);
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 int CALLBACK WinMain(
@@ -22,14 +49,14 @@ int CALLBACK WinMain(
 
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_OWNDC;
+	wc.style = CS_OWNDC | CS_DBLCLKS;
 	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInst;
 	wc.hIcon = nullptr;
 	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
+	wc.hbrBackground =  nullptr;
 	wc.lpszMenuName = nullptr;
 	wc.lpszClassName = className;
 	RegisterClassEx(&wc);
@@ -43,13 +70,13 @@ int CALLBACK WinMain(
 	ShowWindow(hWnd, SW_SHOW);
 
 	MSG msg = { };
-	BOOL k;
-	while ((k = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	BOOL gParam;
+	while ((gParam = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	if (k == -1) return -1;
+	if (gParam == -1) return -1;
 	else return msg.wParam;
 }
