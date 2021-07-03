7af69571d427dc5713fce483e29b12193c5efff6
Mihajlo Srećković <mihajlosrec12@gmail.com>
2021-07-03 17:48:29 +0200

    Camera is being controlled with keyboard

--- Engine/UniverseScene.h
+++ Engine/UniverseScene.h
@@ -13,7 +13,10 @@ public:
 	void Draw() const override;
 private:
 	void SpawnFactory();
+	void ControllCamera(float dt, Keyboard& kbd);
 private:
 	Camera cam;
 	std::vector<ControllableEntity> ctrlbls;
+	float camSpeed = 20.0f;
+	float camRotSpeed = 1.0f;
 };
